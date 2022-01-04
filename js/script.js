b07d49c8a76cfcdf8c6b5f0f20f9faa6e736363b
valencprado <valenpschool@gmail.com>
2022-01-04 20:24:28 -0300

    reveal effect

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+
+
+
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal')
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight
+        var revealtop = reveals[i].getBoundingClientRect().top
+        var revealpoint = 150
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active')
+
+        } else {
+            reveals[i].classList.remove('active')
+        }
+    }
+}

c7046f1ddbdb8d0eb32be77eee01358ab59805e6
valencprado <valenpschool@gmail.com>
2022-01-19 16:39:38 -0300

    animation (failed)

--- js/script.js
+++ js/script.js
@@ -30,3 +30,22 @@ function reveal() {
         }
     }
 }
+/*window.addEventListener('scroll', revealLeft)
+
+function revealLeft() {
+    var reveals = document.querySelectorAll('.reveal-left')
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight
+        var revealtop = reveals[i].getBoundingClientRect().top
+        var revealpoint = 150
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active')
+
+        } else {
+            reveals[i].classList.remove('active')
+        }
+    }
+}
+*/

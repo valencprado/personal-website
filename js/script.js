541dd21f8d4e4afb95e61a509aa3a86ba7f57766
valencprado <valenpschool@gmail.com>
2022-01-20 16:53:46 -0300

    typing animation

--- js/script.js
+++ js/script.js
@@ -30,22 +30,15 @@ function reveal() {
         }
     }
 }
-/*window.addEventListener('scroll', revealLeft)
+var index = 0;
+var text = "Hi! I'm Valentina :)";
+var speed = 50;
 
-function revealLeft() {
-    var reveals = document.querySelectorAll('.reveal-left')
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowheight = window.innerHeight
-        var revealtop = reveals[i].getBoundingClientRect().top
-        var revealpoint = 150
-
-        if (revealtop < windowheight - revealpoint) {
-            reveals[i].classList.add('active')
-
-        } else {
-            reveals[i].classList.remove('active')
-        }
+function textEffect() {
+    if (index < text.length) {
+        document.getElementById("effect")
+            .innerHTML += text.charAt(index);
+        index++;
+        setTimeout(textEffect, speed);
     }
 }
-*/

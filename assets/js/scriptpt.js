5bae3b4e28c7ba0be765bcbcec7f32ddd1c4e916
Valen <valenpschool@gmail.com>
2022-01-29 21:24:31 -0300

    new folder

---
+++ assets/js/scriptpt.js
@@ -0,0 +1,30 @@
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal')
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight
+        var revealtop = reveals[i].getBoundingClientRect().top
+        var revealpoint = 150
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active')
+
+        } else {
+            reveals[i].classList.remove('active')
+        }
+    }
+}
+var index = 0;
+var text = "Oi! Eu sou a Valentina :)";
+var speed = 50;
+
+function textEffect() {
+    if (index < text.length) {
+        document.getElementById("effect")
+            .innerHTML += text.charAt(index);
+        index++;
+        setTimeout(textEffect, speed);
+    }
+}
